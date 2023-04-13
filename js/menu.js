03ecb47a886fbe9731654870c0175921b7aa779e
JoaoVictorCX <joaovictorcoelhox@gmail.com>
2023-04-13 09:34:43 -0300

    button

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,24 @@
 let nav = document.querySelector('.menu-nav')
 let btn = document.querySelector('.btn-menu-mobile')
 let menuitens = document.querySelectorAll('.btn-menu')
+let btMenuOpen = document.querySelector('.menu-open')
+let btMenuClose = document.querySelector('.menu-close')
 
 function toggle(){
     if(nav.classList.contains('showmenu')){
         CloseMenu()
+
     } else {
         nav.classList.add('showmenu')
+        btMenuOpen.style.display = 'none'
+        btMenuClose.style.display = 'flex'
     }
 }
 
 function CloseMenu (){
     nav.classList.remove('showmenu')
+    btMenuOpen.style.display = 'block'
+    btMenuClose.style.display = 'none'
 }
 
 function AddEventsMenu (item){
